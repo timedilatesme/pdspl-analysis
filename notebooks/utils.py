32fb97425270106c16ac0f6301069a96ce3f0c89
Paras Sharma <64778535+timedilatesme@users.noreply.github.com>
2025-10-08 01:44:35 -0400

    .

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,8 @@
 from astropy.table import Table
 import numpy as np
 from itertools import combinations
+from hierarc.Likelihood.LensLikelihood.double_source_plane import beta2theta_e_ratio, beta_double_source_plane
+# from lenstronomy.LensModel.lens_model import LensModel
 
 #############################################################################
 # PLANE FITTING AND SCATTER
@@ -46,32 +48,32 @@ def find_scatter(x, y, z, coeffs, return_fit=False):
 ############################################################################
 # PAIRING SIMULATION
 ############################################################################
-def generate_fake_catalog(source_table, num_samples):
-    """
-    Generates a fake galaxy catalog by resampling rows from a source table
-    and computes the number of pairs on the fundamental plane.
+# def generate_fake_catalog(source_table, num_samples):
+#     """
+#     Generates a fake galaxy catalog by resampling rows from a source table
+#     and computes the number of pairs on the fundamental plane.
 
-    Args:
-        source_table (astropy.table.Table): The original data table to resample from.
-        num_samples (int): The number of galaxies to generate for the catalog.
+#     Args:
+#         source_table (astropy.table.Table): The original data table to resample from.
+#         num_samples (int): The number of galaxies to generate for the catalog.
 
-    Returns:
-        astropy.table.Table: The generated fake galaxy catalog.
-    """
-    indices = np.random.choice(len(source_table), size=num_samples, replace=True)
-    fake_data = source_table[indices]
+#     Returns:
+#         astropy.table.Table: The generated fake galaxy catalog.
+#     """
+#     indices = np.random.choice(len(source_table), size=num_samples, replace=True)
+#     fake_data = source_table[indices]
 
-    # remove the 'lens_id' column if it exists
-    if 'lens_id' in fake_data.colnames:
-        fake_data.remove_column('lens_id')
+#     # remove the 'lens_id' column if it exists
+#     if 'lens_id' in fake_data.colnames:
+#         fake_data.remove_column('lens_id')
 
-    # for each lens perturb the parameters a little bit by 1% of std of each parameter
-    for col in fake_data.colnames:
-        col_std = np.std(source_table[col])
-        if not col_std == 0:
-            fake_data[col] += np.random.normal(0, col_std, num_samples)*0.01
+#     # for each lens perturb the parameters a little bit by 1% of std of each parameter
+#     for col in fake_data.colnames:
+#         col_std = np.std(source_table[col])
+#         if not col_std == 0:
+#             fake_data[col] += np.random.normal(0, col_std, num_samples)*0.01
 
-    return fake_data
+#     return fake_data
 
 # def generate_fake_catalog(source_table, num_samples):
 #     """
@@ -93,8 +95,8 @@ def generate_fake_catalog(source_table, num_samples):
 
 #     return fake_data
 
-def run_pairing_simulation_from_data(data, mag_limit=22, threshold_rel_delta_z=0.01, 
-                                     verbose=True, is_source_cut=False):
+def run_pairing_simulation_from_data(data, threshold_rel_delta_z=0.01, 
+                                     verbose=True):
     """
     Computes the number of pairs on the fundamental plane.
 
@@ -105,19 +107,16 @@ def run_pairing_simulation_from_data(data, mag_limit=22, threshold_rel_delta_z=0
         threshold_rel_delta_z (float): The relative redshift difference threshold for a valid pair.
 
     Returns:
-        int: The total number of valid pairs found.
+        int: The number of valid pairs found.
+        astropy.table.Table: Table of valid pairs with columns 'lens_id_1' and 'lens_id_2'.
+
     """
     if verbose:
         print(f"\n--- Running simulation for {len(data)} lenses ---")
 
-    data = data.copy()
 
     # Step 1: Use the data
-    data = data[data['mag_D_r'] < mag_limit] # Apply magnitude limit for deflector
-    if is_source_cut:
-        data = data[data['mag_S_r'] < mag_limit] # Apply magnitude limit for source
-    if verbose:
-        print(f"Number of samples after magnitude cut: {len(data)}")
+    data = data.copy()
     if len(data) < 2:
         return 0
 
@@ -134,8 +133,8 @@ def run_pairing_simulation_from_data(data, mag_limit=22, threshold_rel_delta_z=0
     # determine the bin_widths based on the data's stddev
     x_std = np.nanstd(x)
     y_std = np.nanstd(y)
-    x_width_bin = 3.5 * x_std / (len(x)**(1/(2+2)))
-    y_width_bin = 3.5 * y_std / (len(y)**(1/(2+2)))
+    x_width_bin = 3.5 * x_std / (len(x)**(1/(2+2))) # Scott's rule
+    y_width_bin = 3.5 * y_std / (len(y)**(1/(2+2))) # Scott's rule
     num_bins_x = int((np.nanmax(x) - np.nanmin(x)) / x_width_bin) + 1
     num_bins_y = int((np.nanmax(y) - np.nanmin(y)) / y_width_bin) + 1
 
@@ -148,6 +147,8 @@ def run_pairing_simulation_from_data(data, mag_limit=22, threshold_rel_delta_z=0
     y_bins = np.linspace(np.nanmin(y), np.nanmax(y), num_bins_y)
 
     total_pairs = 0
+    valid_pairs_idx1 = []
+    valid_pairs_idx2 = []
     
     # Iterate through each bin on the plane
     if verbose:
@@ -171,25 +172,92 @@ def run_pairing_simulation_from_data(data, mag_limit=22, threshold_rel_delta_z=0
                     continue
                 if 2 * np.abs(lens1['z_D'] - lens2['z_D']) / (lens1['z_D'] + lens2['z_D']) <= threshold_rel_delta_z:
                     total_pairs += 1
+                    valid_pairs_idx1.append(lens1['lens_id'])
+                    valid_pairs_idx2.append(lens2['lens_id'])
     if verbose:
         print(f"Found {total_pairs} pairs.")
+    
+    # astropy table of valid pairs
+    valid_pairs_table = Table()
+    valid_pairs_table['lens_id_1'] = valid_pairs_idx1
+    valid_pairs_table['lens_id_2'] = valid_pairs_idx2
 
-    return total_pairs
+    return total_pairs, valid_pairs_table
 
-def run_pairing_simulation(num_samples, source_table, mag_limit=22, threshold_rel_delta_z=0.01, 
-                           verbose=True, is_source_cut=False):
-    # Generates a fake galaxy catalog by resampling rows from a source table
-    fake_data = generate_fake_catalog(
-        source_table=source_table,
-        num_samples=num_samples,
-    )
+def pdspl_pairing_table_from_valid_pairs_table(valid_pairs_table, full_data_table, cosmo_true):
+    """
+    Given a table of valid pairs (with columns 'lens_id_1' and 'lens_id_2'),
+    return a full pairing table with all properties of the lenses.
 
-    # Step 2: Run the pairing simulation on the fake data
-    return run_pairing_simulation_from_data(
-        data=fake_data,
-        mag_limit=mag_limit,
-        threshold_rel_delta_z=threshold_rel_delta_z,
-        verbose=verbose,
-        is_source_cut=is_source_cut
-    )
+    Args:
+        valid_pairs_table (astropy.table.Table): Table of valid pairs with columns 'lens_id_1' and 'lens_id_2'.
+        full_data_table (astropy.table.Table): The full data table containing all lens properties.
+
+    Returns:
+        astropy.table.Table: Full pairing table with properties of both lenses in each pair.
+    """
+    individual_pair_data = {
+        'beta_E_pseudo': [],
+        'beta_E_DSPL': [],
+        'z_D1': [],
+        'z_D2': [],
+        'z_S1': [],
+        'z_S2': [],
+        'sigma_v_D1': [],
+        'sigma_v_D2': [],
+        'lens1_id': [],
+        'lens2_id': [],
+        }
+
+    for lens1_id, lens2_id in zip(valid_pairs_table['lens_id_1'], valid_pairs_table['lens_id_2']):
+        lens1 = full_data_table[full_data_table['lens_id'] == lens1_id][0]
+        lens2 = full_data_table[full_data_table['lens_id'] == lens2_id][0]
+        beta_E_pseudo = lens1['theta_E'] / lens2['theta_E']
+
+        # check z_lens < z_source
+        if lens1['z_D'] >= lens2['z_S'] or lens2['z_D'] >= lens1['z_S']:
+            continue
+
+        beta_DSPL = beta_double_source_plane(
+            z_lens = np.mean([lens1['z_D'], lens2['z_D']]),
+            z_source_1= lens1['z_S'],
+            z_source_2= lens2['z_S'],
+            cosmo = cosmo_true,
+        )
+        beta_E_DSPL = beta2theta_e_ratio(
+            beta_dsp= beta_DSPL,
+            gamma_pl= 2, #TODO: what if gamma_pl != 2?
+            lambda_mst= 1
+        )
+
+        individual_pair_data['beta_E_pseudo'].append(beta_E_pseudo)
+        individual_pair_data['beta_E_DSPL'].append(beta_E_DSPL)
+        individual_pair_data['z_D1'].append(lens1['z_D'])
+        individual_pair_data['z_D2'].append(lens2['z_D'])
+        individual_pair_data['z_S1'].append(lens1['z_S'])
+        individual_pair_data['z_S2'].append(lens2['z_S'])
+        individual_pair_data['sigma_v_D1'].append(lens1['sigma_v_D'])
+        individual_pair_data['sigma_v_D2'].append(lens2['sigma_v_D'])
+        individual_pair_data['lens1_id'].append(lens1['lens_id'])
+        individual_pair_data['lens2_id'].append(lens2['lens_id'])
+    
+    pairing_table = Table(individual_pair_data)
+    return pairing_table
+
+# def run_pairing_simulation(num_samples, source_table, mag_limit=22, threshold_rel_delta_z=0.01, 
+#                            verbose=True, is_source_cut=False):
+#     # Generates a fake galaxy catalog by resampling rows from a source table
+#     fake_data = generate_fake_catalog(
+#         source_table=source_table,
+#         num_samples=num_samples,
+#     )
+
+#     # Step 2: Run the pairing simulation on the fake data
+#     return run_pairing_simulation_from_data(
+#         data=fake_data,
+#         mag_limit=mag_limit,
+#         threshold_rel_delta_z=threshold_rel_delta_z,
+#         verbose=verbose,
+#         is_source_cut=is_source_cut
+#     )
 #############################################################################
